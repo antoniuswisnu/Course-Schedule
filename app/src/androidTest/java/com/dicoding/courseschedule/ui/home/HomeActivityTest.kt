771534d29935a42d82249793fd1b31d53c9b5711
Antonius Wisnu <antonius.kri2003@mail.ugm.ac.id>
2023-11-29 16:03:28 +0700

    Initial commit

---
+++ app/src/androidTest/java/com/dicoding/courseschedule/ui/home/HomeActivityTest.kt
@@ -0,0 +1,44 @@
+package com.dicoding.courseschedule.ui.home
+
+import android.app.Activity
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.runner.lifecycle.ActivityLifecycleMonitorRegistry
+import androidx.test.runner.lifecycle.Stage
+import com.dicoding.courseschedule.R
+import com.dicoding.courseschedule.ui.add.AddCourseActivity
+import org.junit.Assert
+import org.junit.Rule
+import org.junit.Test
+
+class HomeActivityTest {
+    @get:Rule
+    var activityRule = ActivityScenarioRule(HomeActivity::class.java)
+
+    @Test
+    fun loadAddCourse(){
+
+        Espresso.onView(ViewMatchers.withId(R.id.action_add)).apply {
+            check(ViewAssertions.matches(ViewMatchers.isCompletelyDisplayed()))
+            perform(ViewActions.click())
+        }
+
+        val addCourse = getAddCourseActivity()
+        Assert.assertTrue(addCourse?.javaClass == AddCourseActivity::class.java)
+    }
+
+    private fun getAddCourseActivity(): Activity? {
+        var activity: Activity? = null
+        InstrumentationRegistry.getInstrumentation().runOnMainSync {
+            run {
+                activity = ActivityLifecycleMonitorRegistry.getInstance()
+                    .getActivitiesInStage(Stage.RESUMED).elementAtOrNull(0)
+            }
+        }
+        return activity
+    }
+}
